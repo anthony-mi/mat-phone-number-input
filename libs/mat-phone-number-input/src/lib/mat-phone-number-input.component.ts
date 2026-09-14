2691f851012d0798690a32bb5c1127a628fe184e
Anton Mynchenko <anton.minchenko@sigma.software>
2026-09-14 15:58:24 +0200

    Fall back to default country when parsing an invalid-country phone number

--- libs/mat-phone-number-input/src/lib/mat-phone-number-input.component.ts
+++ libs/mat-phone-number-input/src/lib/mat-phone-number-input.component.ts
@@ -41,6 +41,7 @@ import {
   NationalNumber,
   PhoneNumber,
   getExampleNumber,
+  parsePhoneNumber,
   parsePhoneNumberFromString,
 } from 'libphonenumber-js';
 import { Subject } from 'rxjs';
@@ -454,7 +455,7 @@ export class MatPhoneNumberInput
 
   writeValue(value: any): void {
     if (value) {
-      this.numberInstance = parsePhoneNumberFromString(value);
+      this.numberInstance = this.getPhoneNumberInstance(value);
       if (this.numberInstance) {
         const countryCode = this.numberInstance.country;
         this.phoneNumber = this.formattedPhoneNumber();
@@ -504,6 +505,21 @@ export class MatPhoneNumberInput
 
     this._changeDetectorRef.markForCheck();
     this.stateChanges.next(undefined);
+  }
+
+  private getPhoneNumberInstance(value: string): PhoneNumber | undefined {
+    try {
+      parsePhoneNumber(value);
+    } catch(e: any) {
+      if (e['message'] === 'INVALID_COUNTRY') {
+        this._setDefaultCountry();
+        this.numberInstance = parsePhoneNumberFromString(this.ngControl.control!.value, this.selectedCountry.iso2.toUpperCase() as CC);
+        value = this.numberInstance?.number || value;
+        this.ngControl.control?.setValue(value, { emitEvent: false });
+      }
+    }
+
+    return parsePhoneNumberFromString(value);
   }
 
   private formattedPhoneNumber(): E164Number | NationalNumber {
